f3bc583fc26246079ec4fd2010d1bf452f8ca8fc
Jack Compton <jack.compton@gmail.com>
2015-04-16 16:18:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var dc = require('dc'),
+    jQuery = require('jquery');
+var $ = jQuery;
+
+var dcCustom = {
+  filterTrail: require('./filter-trail'),
+  filterBuilder: require('./filter-builder'),
+  selectControl: require('./select-control'),
+  dynatableComponent: require('./dynatable-component'),
+  datatablesComponent: require('./datatables-component'),
+  audioDash: require('./audio-dash')
+}
+
+jQuery.extend(dc, dcCustom);
+
+module.exports = dc;
